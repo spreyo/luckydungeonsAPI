d9939817ecab79521315bb011ce137a941203426
spreyo <musicgamermail@gmail.com>
2023-11-04 12:20:47 +0100

    log the username and amount

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ app.post('/deposit', (req, res, next) => {
     username = req.body["username"];
     amount = req.body.amount;
     console.log(username);
-    res.send(username);
+    res.send(username, amount);
     res.sendStatus(200);
 
 })
